cc5d12b33a428952fae194bb61566fb80eaa00cb
eoinmcbennett <eoinmcbennett12345@gmail.com>
2023-11-13 11:11:31 +0000

    Changed to air bnbs ruleset

--- app.ts
+++ app.ts
@@ -1,29 +1,30 @@
-import {Request, Response} from "express"
-import express = require("express");
-import path = require('path')
-import nunjucks = require('nunjucks')
+import { Request, Response } from 'express';
+import express = require('express');
+import path = require('path');
+import nunjucks = require('nunjucks');
 
 const app = express();
 
-const appViews = path.join(__dirname, '/views')
+const appViews = path.join(__dirname, '/views');
 
-const nunjucksConfig={
-    autoescape: true,
-    noCache: true,
-    express: app
-}
+const nunjucksConfig = {
+  autoescape: true,
+  noCache: true,
+  express: app,
+};
 
-nunjucks.configure(appViews, nunjucksConfig)
+nunjucks.configure(appViews, nunjucksConfig);
 
-app.set('view engine', 'html')
-app.use('/public', express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.use (express.json())
-app.use(express.urlencoded({ extended: true}))
-app.listen(3000, () =>{
-    console.log('Server listening on port 3000')
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async(req: Request, res: Response) =>{
-    res.render('index');
-})
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
+
+app.get('/', async (req: Request, res: Response) => {
+  res.render('index');
+});
