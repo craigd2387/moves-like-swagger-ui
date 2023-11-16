a8b98ba5b75dae2c94509483de08829087ec3e9f
Michael Clarke <michael.clarke@kainos.com>
2023-11-16 14:37:20 +0000

    edited selenium

--- test/unit/service/ui/login-page.test.ts
+++ test/unit/service/ui/login-page.test.ts
@@ -1,99 +1,65 @@
+import { Builder, By, until, WebDriver } from 'selenium-webdriver';
 import { expect } from 'chai';
-import * as webdriver from 'selenium-webdriver';
-import 'selenium-webdriver/chrome';
-import 'selenium-webdriver/firefox';
-import 'selenium-webdriver/edge';
-import 'selenium-webdriver/safari';
+import * as chrome from 'selenium-webdriver/chrome';
 
-let driver: webdriver.WebDriver;
+describe('Login Page', function() {
+  this.timeout(100000);
+ 
+  let driver: WebDriver;
 
-before(function (done) {
-    const timeout = 3000;
-
-    setTimeout(function () {
-        try {
-            driver = new webdriver.Builder()
-                .withCapabilities(webdriver.Capabilities.chrome())
-                .build();
-
-            driver.manage().window().maximize();
-
-            console.log('WebDriver successfully initialized.');
-            done();
-        } catch (error) {
-            console.error('Error during WebDriver initialization:', error);
-            done(error);
-        }
-    }, timeout);
-});
-
-afterEach(async function (done) {
-    try {
-      let testCaseName: string = this.currentTest.title;
-      let testCaseStatus: string = this.currentTest.state;
-  
-      if (testCaseStatus === 'failed') {
-        console.log(`Test: ${testCaseName}, Status: Failed!`);
-
-      } else if (testCaseStatus === 'passed') {
-        console.log(`Test: ${testCaseName}, Status: Passed!`);
-      } else {
-        console.log(`Test: ${testCaseName}, Status: Unknown!`);
-      }
-  
-      done();
-    } catch (error) {
-      console.error('Error during afterEach:', error);
-      done(error);
-    }
+  before(async function() {
+    const options = new chrome.Options();
+    options.addArguments('--headless');
+    options.addArguments('--window-size=1920,1080');
+    driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
   });
 
-after(function () {
+    it('should log in successfully', async function () {
+      const Url: string = 'http://localhost:3000/login';
+      await driver.get(Url);
+
+      console.log(`Page "${Url}" opened`);
+
+      const currentUrl = await driver.getCurrentUrl();
+      expect(currentUrl).to.include('http://localhost:3000/login');
+
+      const usernameBox = await driver.findElement(By.id('username'));
+      await usernameBox.sendKeys('test@kainos.com');
+
+      const passwordBox = await driver.findElement(By.id('password'));
+      await passwordBox.sendKeys('testing');
+
+      const loginButton = await driver.findElement(By.id('submitbutton'));
+      await loginButton.click();
+
+      const finalUrl = await driver.getCurrentUrl();
+      expect(finalUrl).to.include('http://localhost:3000/');
+  });
+
+    it('should display error message with incorrect login credentials', async function () {
+      const Url: string = 'http://localhost:3000/login';
+      await driver.get(Url);
+
+      console.log(`Page "${Url}" opened`);
+
+      const currentUrl = await driver.getCurrentUrl();
+      expect(currentUrl).to.include('http://localhost:3000/login');
+
+      const usernameBox = await driver.findElement(By.id('username'));
+      await usernameBox.sendKeys('false-email');
+
+      const passwordBox = await driver.findElement(By.id('password'));
+      await passwordBox.sendKeys('test');
+
+      const loginButton = await driver.findElement(By.id('submitbutton'));
+      await loginButton.click();
+
+      const errorMessageElement = await driver.findElement(By.id('errormessage'));
+      const errorMessageText = await errorMessageElement.getText();
+      expect(errorMessageText).to.equal('Invalid Credentials');
+  });
+
+  after(async function () {
     driver.quit();
-});
-
-//test
-it('should log in successfully', async function () {
-    const Url: string = 'http://localhost:3000/login';
-    await driver.get(Url);
-
-    console.log(`Page "${Url}" opened`);
-
-    const currentUrl = await driver.getCurrentUrl();
-    expect(currentUrl).to.include('http://localhost:3000/login');
-
-    const usernameBox = await driver.findElement(webdriver.By.id('username'));
-    await usernameBox.sendKeys('test@kainos.com');
-
-    const passwordBox = await driver.findElement(webdriver.By.id('password'));
-    await passwordBox.sendKeys('testing');
-
-    const loginButton = await driver.findElement(webdriver.By.xpath('//*[@id="main"]/form/button'));
-    await loginButton.click();
-
-    const finalUrl = await driver.getCurrentUrl();
-    expect(finalUrl).to.include('http://localhost:3000/');
-});
-
-it('should display error message with incorrect login credentials', async function () {
-    const Url: string = 'http://localhost:3000/login';
-    await driver.get(Url);
-
-    console.log(`Page "${Url}" opened`);
-
-    const currentUrl = await driver.getCurrentUrl();
-    expect(currentUrl).to.include('http://localhost:3000/login');
-
-    const usernameBox = await driver.findElement(webdriver.By.id('username'));
-    await usernameBox.sendKeys('false-email');
-
-    const passwordBox = await driver.findElement(webdriver.By.id('password'));
-    await passwordBox.sendKeys('test');
-
-    const loginButton = await driver.findElement(webdriver.By.xpath('//*[@id="main"]/form/button'));
-    await loginButton.click();
-
-    const errorMessageElement = await driver.findElement(webdriver.By.className('alert alert-danger'));
-    const errorMessageText = await errorMessageElement.getText();
-    expect(errorMessageText).to.equal('Invalid Credentials');
+    });
 });
