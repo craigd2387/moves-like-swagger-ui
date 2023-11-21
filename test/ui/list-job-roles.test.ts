417d3f43b84c59dad10feea2cb538253a82d9004
Michael Clarke <michael.clarke@kainos.com>
2023-11-21 11:18:44 +0000

    ui changes and testing

--- test/ui/list-job-roles.test.ts
+++ test/ui/list-job-roles.test.ts
@@ -1,45 +1,67 @@
-import { Builder, By, until, WebDriver } from 'selenium-webdriver';
-import { expect } from 'chai';
-import * as chrome from 'selenium-webdriver/chrome';
+// import { Builder, By, until, WebDriver } from 'selenium-webdriver';
+// import { expect } from 'chai';
+// import * as chrome from 'selenium-webdriver/chrome';
  
-describe('Landing Page', function() {
-  this.timeout(100000);
+// describe('Job Roles', function() {
+//   this.timeout(100000);
  
-  let driver: WebDriver;
+//   let driver: WebDriver;
  
-  before(async function() {
-    const options = new chrome.Options();
-    options.addArguments('--headless');
-    options.addArguments('--window-size=1920,1080');
-    driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
-    await driver.get('https://mjz7ptyppt.eu-west-1.awsapprunner.com');
-  });
- 
-  describe('"View Job Roles" button', function() {
-    it('should load job list when clicked', async function() {
-      const button = await driver.findElement(By.id('viewJobRolesButton'));
-      await new Promise(resolve => setTimeout(resolve, 10000));
-      await button.click();
+//   before(async function() {
+//     const options = new chrome.Options();
+//     options.addArguments('--headless');
+//     options.addArguments('--window-size=1920,1080');
+//     driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
+//     await driver.get('http://localhost:3000');
+//   });
+
+//   describe('Job Roles', function() {
+//     it('should redirect to log in page', async function() {
+//       const button = await driver.findElement(By.id('viewJobRolesButton'));
+//       await new Promise(resolve => setTimeout(resolve, 10000));
+//       await button.click();
       
-      const jobTable = await driver.wait(until.elementLocated(By.id('jobs')), 30000);
-      expect(await jobTable.isDisplayed()).to.be.true;
-    });
-  });
+//       const jobTable = await driver.wait(until.elementLocated(By.id('jobsLink')), 30000);
+//       expect(await jobTable.isDisplayed()).to.be.true;
+//     });
+//   });
+
+//   describe('Log in', function() {
+//     it('should log in before allowing job access', async function() {
+//       const button = await driver.findElement(By.id('viewJobRolesButton'));
+//       await new Promise(resolve => setTimeout(resolve, 10000));
+//       await button.click();
+      
+//       const jobTable = await driver.wait(until.elementLocated(By.id('jobsLink')), 30000);
+//       expect(await jobTable.isDisplayed()).to.be.true;
+//     });
+//   });
  
-  describe('"First View in Sharepoint" button', function() {
-    it('should redirect to the appropriate page when clicked', async function() {
-      const buttons = await driver.findElements(By.id('viewSharePointButton'));
-      const sharepointButton = buttons[0];
-      await sharepointButton.click();
+//   describe('"View Job Roles" button', function() {
+//     it('should load job list when clicked', async function() {
+//       const button = await driver.findElement(By.id('viewJobRolesButton'));
+//       await new Promise(resolve => setTimeout(resolve, 10000));
+//       await button.click();
+      
+//       const jobTable = await driver.wait(until.elementLocated(By.id('jobsLink')), 30000);
+//       expect(await jobTable.isDisplayed()).to.be.true;
+//     });
+//   });
  
-      await driver.wait(until.urlContains('sharepoint'), 20000);
+//   describe('"First View in Sharepoint" button', function() {
+//     it('should redirect to the appropriate page when clicked', async function() {
+//       const buttons = await driver.findElements(By.id('viewSharePointButton'));
+//       const sharepointButton = buttons[0];
+//       await sharepointButton.click();
  
-      const url = await driver.getCurrentUrl();
-      expect(url).to.include('sharepoint');
-    });
-  });
+//       await driver.wait(until.urlContains('sharepoint'), 20000);
  
-  after(async function() {
-    await driver.quit();
-  });
-});
+//       const url = await driver.getCurrentUrl();
+//       expect(url).to.include('sharepoint');
+//     });
+//   });
+ 
+//   after(async function() {
+//     await driver.quit();
+//   });
+// });
--- test/ui/login-page.test.ts
+++ test/ui/login-page.test.ts
@@ -2,7 +2,7 @@ import { Builder, By, until, WebDriver } from 'selenium-webdriver';
 import { expect } from 'chai';
 import * as chrome from 'selenium-webdriver/chrome';
 
-describe('Login Page', function() {
+describe('Home Page', function() {
   this.timeout(100000);
  
   let driver: WebDriver;
@@ -14,14 +14,15 @@ describe('Login Page', function() {
     driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
   });
 
+  describe('Log in', function() {
     it('should log in successfully', async function () {
-      const Url: string = 'https://mjz7ptyppt.eu-west-1.awsapprunner.com/login';
+      const Url: string = 'http://localhost:3000/login';
       await driver.get(Url);
 
       console.log(`Page "${Url}" opened`);
 
       const currentUrl = await driver.getCurrentUrl();
-      expect(currentUrl).to.include('https://mjz7ptyppt.eu-west-1.awsapprunner.com/login');
+      expect(currentUrl).to.include('http://localhost:3000/login');
 
       const usernameBox = await driver.findElement(By.id('username'));
       await usernameBox.sendKeys('test@kainos.com');
@@ -31,33 +32,38 @@ describe('Login Page', function() {
 
       const loginButton = await driver.findElement(By.id('submitbutton'));
       await loginButton.click();
+  });
 
+    it('should redirect to homepage', async function () {
       const finalUrl = await driver.getCurrentUrl();
-      expect(finalUrl).to.include('https://mjz7ptyppt.eu-west-1.awsapprunner.com');
+      expect(finalUrl).to.include('http://localhost:3000');
+  });
+});
+
+describe('View Job Roles functionality', function() {
+  it('should load job list when logged in', async function() {
+
+    const button = await driver.findElement(By.id('jobsLink'));
+    await new Promise(resolve => setTimeout(resolve, 10000));
+    await button.click();
+
+    const currentUrl = await driver.getCurrentUrl();
+    expect(currentUrl).to.include('http://localhost:3000/jobs');
+    
+    const jobTable = await driver.wait(until.elementLocated(By.id('jobRolesTable')), 30000);
+    expect(await jobTable.isDisplayed()).to.be.true;
   });
 
-    it('should display error message with incorrect login credentials', async function () {
-      const Url: string = 'https://mjz7ptyppt.eu-west-1.awsapprunner.com/login';
-      await driver.get(Url);
+  it('should return to homepage via home button', async function() {
 
-      console.log(`Page "${Url}" opened`);
+    const button = await driver.findElement(By.id('homeButton'));
+    await new Promise(resolve => setTimeout(resolve, 10000));
+    await button.click();
 
-      const currentUrl = await driver.getCurrentUrl();
-      expect(currentUrl).to.include('https://mjz7ptyppt.eu-west-1.awsapprunner.com/login');
-
-      const usernameBox = await driver.findElement(By.id('username'));
-      await usernameBox.sendKeys('false-email');
-
-      const passwordBox = await driver.findElement(By.id('password'));
-      await passwordBox.sendKeys('test');
-
-      const loginButton = await driver.findElement(By.id('submitbutton'));
-      await loginButton.click();
-
-      const errorMessageElement = await driver.findElement(By.id('errormessage'));
-      const errorMessageText = await errorMessageElement.getText();
-      expect(errorMessageText).to.equal('Invalid Credentials');
+    const currentUrl = await driver.getCurrentUrl();
+    expect(currentUrl).to.include('http://localhost:3000');
   });
+});
 
   after(async function () {
     driver.quit();
