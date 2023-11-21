f26458eb862e0465652ab4b926488a93260cfb72
Michael Clarke <michael.clarke@kainos.com>
2023-11-21 13:52:24 +0000

    edited jobs id

--- test/ui/login-page.test.ts
+++ test/ui/login-page.test.ts
@@ -49,8 +49,8 @@ describe('View Job Roles', function() {
 
   it('should load job list when logged in', async function() {
 
-    const jobsLink = await driver.findElement(By.id('jobsLink'));
-    await new Promise(resolve => setTimeout(resolve, 20000));
+    const jobsLink = await driver.findElement(By.linkText('Jobs'));
+    await new Promise(resolve => setTimeout(resolve, 10000));
     await jobsLink.click();
 
     const currentUrl = await driver.getCurrentUrl();
