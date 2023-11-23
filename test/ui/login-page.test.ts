99ae1a0eaaf5ec2ddb4dbe36a97b199ba2824565
Michael Clarke <michael.clarke@kainos.com>
2023-11-23 09:50:03 +0000

    edited jobs link and selenium test

--- test/ui/login-page.test.ts
+++ test/ui/login-page.test.ts
@@ -61,6 +61,19 @@ describe('View Job Roles', function() {
     }
 });
 
+  it('should direct to jobs page', async function() {
+    try {
+      const jobsLink = await driver.findElement(By.id('jobsLink'));
+        await new Promise(resolve => setTimeout(resolve, 10000));
+        await jobsLink.click();
+
+      const currentUrl = await driver.getCurrentUrl();
+      expect(currentUrl).to.include('http://localhost:3000/jobs');
+    } catch (error) {
+      console.error('Error with jobs link:', error);
+    }
+  });
+
   it('should load job list table when logged in', async function() {
     try {
       const Url: string = 'http://localhost:3000/jobs';
